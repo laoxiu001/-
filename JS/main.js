1e9a561ce69b8c5a93e21557a25d2ff5550405f3
laoxiu <316075960@qq.com>
2018-08-16 23:18:32 +0800

    纯手写懒加载特效完成，整理文档

--- JS/main.js
+++ JS/main.js
@@ -1,35 +1,53 @@
 /* 整个 HTML 页面 淡入淡出 */
-$(document).ready(function(){
+$(document).ready(function () {
     $("html").fadeIn(1000);
-    visible();
+    var top = $(".top").height();
+    visible(top);
 });
 /* 整个 HTML 页面 淡入淡出 */
 
+/* 点击导航栏展开 */
+function showNav() {
+    $(document).ready(function () {
+        $(".nav_xs").slideToggle("slow");
+    })
+}
+/* 点击导航栏展开 */
+
+/* 屏幕变大隐藏div */
+function hideNav() {
+    var width = $(window).width();
+    if (width >= 768) {
+        $(".nav_xs").css("display", "none")
+    }
+}
+/* 屏幕变大隐藏div */
+
 /* 首屏高度自适应 */
-function visible() {
-    $(".GitHub").height($(window).height());
+function visible(height) {
+    $(".visible").height($(window).height() - height);
 }
 $(window).resize(function () {
     //浏览器大小变化
-    visible();
+    var top = $(".top").height();
+    visible(top);
+    hideNav();
 });
 /* 首屏高度自适应 */
 
 /* lazy 懒加载内容 */
-$(window).scroll(function(event){
+$(window).scroll(function (event) {
     var pageTopY = $(window).scrollTop();
     var pageBottomY = $(window).scrollTop() + $(window).height();
 
-    $(".lazy").each(function(){
+    $(".box").each(function () {
         var boxTopY = $(this).offset().top;
         var boxCenter = boxTopY + ($(this).height() / 2);
 
-        if(boxCenter > pageTopY && boxCenter < pageBottomY){
-            $(document).ready(function(){
-                $(".lazy").fadeIn(3000);
-            });
+        if (boxCenter > pageTopY && boxCenter < pageBottomY) {
+                console.log(this);
+                $(this).find('.lazy').fadeIn(3000);
         }
     });
-
 });
 /* lazy 懒加载内容 */
